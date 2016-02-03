99997747270fa019f5f6fe7e739b1c64befeebce
Garrett Richards <garrettrichards@gmail.com>
2016-02-03 15:09:19 -0800

    add tvOS support #44

--- Source/DropboxUtil.swift
+++ Source/DropboxUtil.swift
@@ -60,13 +60,17 @@ public class Dropbox {
     public static func setupWithAppKey(appKey : String) {
         precondition(DropboxAuthManager.sharedAuthManager == nil, "Only call `Dropbox.initAppWithKey` once")
         DropboxAuthManager.sharedAuthManager = DropboxAuthManager(appKey: appKey)
-
+        setupAuthorizedClients()
+    }
+    
+    private static func setupAuthorizedClients() {
         if let token = DropboxAuthManager.sharedAuthManager.getFirstAccessToken() {
             Dropbox.authorizedClient = DropboxClient(accessToken: token)
             DropboxClient.sharedClient = Dropbox.authorizedClient
         }
     }
 
+    #if os(iOS)
     /// Present the OAuth2 authorization request page by presenting a web view controller modally
     ///
     /// - parameter controller: The controller to present from
@@ -74,6 +78,17 @@ public class Dropbox {
         precondition(DropboxAuthManager.sharedAuthManager != nil, "Call `Dropbox.initAppWithKey` before calling this method")
         precondition(Dropbox.authorizedClient == nil, "Client is already authorized")
         DropboxAuthManager.sharedAuthManager.authorizeFromController(controller)
+    }
+    #endif
+    
+    /// Authorize Dropbox client with access token
+    ///
+    /// - parameter accessToken: The access token to authorize client
+    public static func authorizeWithAccessToken(accessToken:DropboxAccessToken) {
+        precondition(DropboxAuthManager.sharedAuthManager != nil, "Call `Dropbox.initAppWithKey` before calling this method")
+        precondition(Dropbox.authorizedClient == nil, "Client is already authorized")
+        DropboxAuthManager.sharedAuthManager.storeAccessToken(accessToken)
+        setupAuthorizedClients()
     }
 
     /// Handle a redirect and automatically initialize the client and save the token.
