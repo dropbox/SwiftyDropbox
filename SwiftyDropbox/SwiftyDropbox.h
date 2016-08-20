4fb74cf8f840aa1d46476bc01b21bd58891bbf89
Alfonso Roman <alroman@ucla.edu>
2016-08-20 16:55:28 -0700

    adding carthage support

---
+++ SwiftyDropbox/SwiftyDropbox.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDropbox.h
+//  SwiftyDropbox
+//
+//  Created by Rob Nash on 2016-03-31.
+//  Copyright © 2016 Robert Nash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDropbox.
+FOUNDATION_EXPORT double SwiftyDropboxVersionNumber;
+
+//! Project version string for SwiftyDropbox.
+FOUNDATION_EXPORT const unsigned char SwiftyDropboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDropbox/PublicHeader.h>
+
+#import "SecurityUtil.h"
