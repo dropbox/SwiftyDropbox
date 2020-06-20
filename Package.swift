8410e5a6f40ea9c64c8db0b2b188c6346dcbae96
Ben Gottlieb <ben@standalone.com>
2020-06-20 05:36:25 -0500

    re-arranged Package arguments

--- Package.swift
+++ Package.swift
@@ -8,15 +8,15 @@ import PackageDescription
 let package = Package(
     name: "SwiftyDropbox",
 	platforms: [.iOS(.v12)],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "4.8.2")),
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftyDropbox",
             targets: ["SwiftyDropbox"]),
 	],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "4.8.2")),
+    ],
 	targets: [
 		.target(name: "SwiftyDropbox", dependencies: ["AlamoFire"])
 	]
