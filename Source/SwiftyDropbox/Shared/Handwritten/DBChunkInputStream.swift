ec636d6693bb8bf12595046eea5affea30a62c79
Ben Gottlieb <ben@standalone.com>
2020-06-20 05:53:29 -0500

    began switching DBChunkInputStream to swift

---
+++ Source/SwiftyDropbox/Shared/Handwritten/DBChunkInputStream.swift
@@ -0,0 +1,14 @@
+//
+//  DBChunkInputStream.swift
+//  
+//
+//  Created by ben on 6/20/20.
+//
+
+import Foundation
+
+class DBChunkInputStream: InputStream {
+	init(fileUrl: URL, startBytes: UInt, endBytes: UInt) {
+		super.init()
+	}
+}
