2118f04ede309833a6a9d804896a64a22d78747f
Ben Gottlieb <ben@standalone.com>
2020-06-20 05:35:21 -0500

    added target to package.swift

--- Package.swift
+++ Package.swift
@@ -11,6 +11,12 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "4.8.2")),
     ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftyDropbox",
+            targets: ["SwiftyDropbox"]),
+	],
 	targets: [
 		.target(name: "SwiftyDropbox", dependencies: ["AlamoFire"])
 	]
