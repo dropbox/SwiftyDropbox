90415fd41b68495d7da4a091fcadc757418db886
Ben Gottlieb <ben@standalone.com>
2020-06-20 10:53:25 -0500

    tweaked xcodeproj and headers

--- Source/SwiftyDropbox/Shared/Handwritten/SwiftyDropbox.h
+++ Source/SwiftyDropbox/Shared/Handwritten/SwiftyDropbox.h
@@ -20,5 +20,3 @@ FOUNDATION_EXPORT double SwiftyDropboxVersionNumber;
 FOUNDATION_EXPORT const unsigned char SwiftyDropboxVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftyDropbox/PublicHeader.h
-
-#import "DBChunkInputStream.h"
