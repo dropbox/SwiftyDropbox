369ccf2c34e16a38fb57aff6fcab4ee9fe1a84f0
Jeff Forbes <jforbes@dropbox.com>
2021-03-19 00:40:46 -0400

    Undo an errant comment.

--- Source/SwiftyDropbox/Platform/SwiftyDropbox_iOS/LoadingViewController.swift
+++ Source/SwiftyDropbox/Platform/SwiftyDropbox_iOS/LoadingViewController.swift
@@ -20,7 +20,7 @@ class LoadingViewController: UIViewController {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
 
-    //@available(*, unavailable, message: "init(coder:) has not been implemented")
+    @available(*, unavailable, message: "init(coder:) has not been implemented")
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
