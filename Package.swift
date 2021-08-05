ed6f0e88af703342f476504dfa5a7816b2fadf37
YufeiG <1757898+YufeiG@users.noreply.github.com>
2021-08-04 22:27:46 -0400

    Update spm config

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 ///
 /// Copyright (c) 2016 Dropbox, Inc. All rights reserved.
 ///
@@ -7,11 +7,15 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyDropbox",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+    ],
     products: [
         .library(name: "SwiftyDropbox", targets:["SwiftyDropbox"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.4.3")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.3")),
     ],
     targets: [
         .target(
