16e243b20a496a127822db4a450f229f80da0fca
Julian Locke <jlocke@dropbox.com>
2024-03-15 15:27:44 -0400

    10.0.5 release.

--- Source/SwiftyDropbox/Shared/Handwritten/SDKConstants.swift
+++ Source/SwiftyDropbox/Shared/Handwritten/SDKConstants.swift
@@ -5,6 +5,6 @@
 import Foundation
 
 public struct Constants {
-    public static let versionSDK = "10.0.4"
+    public static let versionSDK = "10.0.5"
     static let kCSRFKey = "kCSRFKeySwiftSDK"
 }
